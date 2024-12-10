b16a04c6ad044c8a38e360829a25b462cf7c1935
ElliotJoyce09 <elliot.hill000@gmail.com>
2024-12-10 11:35:56 +0000

    added function to go through all games

--- data_cleaning.R
+++ data_cleaning.R
@@ -25,6 +25,8 @@ if (!require(statsbombCleaning)) {
   library(statsbombCleaning)
 }
 
+library(hms)
+
 competitions <- FreeCompetitions() %>%
   filter(!is.na(match_available_360))
 
@@ -45,8 +47,16 @@ euro2024_360data <- free_allevents_360(euro2024matches, Parallel = TRUE) %>%
 merged_dataframe <- join_360_data(cleaned_dataframe, euro2024_360data)
 
 measure_of_defensive_area_occupied <- function(dataframe) {
-  # retrieves the maximum timestamp value of the first half
-  max_timestamp <- max(filter(dataframe, period == 1)$timestamp)
+  dataframe <- dataframe %>%
+    filter(period != 5)
+  # retrieves the maximum timestamp value of the first half, second half and extra time first half (if needed)
+  timestamp_1 <- max(filter(dataframe, period == 1)$timestamp)
+  if (length(unique(dataframe$period)) > 2) {
+    max_timestamp_2 <- max(filter(dataframe, period == 2)$timestamp)
+    timestamp_2 <- as_hms(as.POSIXct(timestamp_1, format = "%H:%M:%OS")) + as_hms(as.POSIXct(max_timestamp_2, format = "%H:%M:%OS"))
+    max_timestamp_3 <- max(filter(dataframe, period == 3)$timestamp)
+    timestamp_3 <- timestamp_2 + as_hms(as.POSIXct(max_timestamp_3, format = "%H:%M:%OS"))
+  }
   # retrieves the two teams playing in the game
   team_names <- unique(dataframe$team.name)
   # a dataframe of zones
@@ -58,40 +68,127 @@ measure_of_defensive_area_occupied <- function(dataframe) {
     y_max = c(16.3, 16.3, 16.3, 63.6, 80, 80, 80, 63.6, 29.2, 50.8, 80, 80, 80)
   )
   # a dataframe of the scaling factors for each zones
-  scaling_factors <- c("1" = 1/8, "2" = 1/16, "3" = 1/32, "4" = 1, "5" = 1/8, "6" = 1/16, "7" = 1/32, "8" = 1/2, "9" = 1/2, "10" = 1/4, "11" = 1/64, "12" = 1/128, "13" = 1/256)
+  scaling_factors <- c(
+    "1" = 1 / 8,
+    "2" = 1 / 16,
+    "3" = 1 / 32,
+    "4" = 1,
+    "5" = 1 / 8,
+    "6" = 1 / 16,
+    "7" = 1 / 32,
+    "8" = 1 / 2,
+    "9" = 1 / 2,
+    "10" = 1 / 4,
+    "11" = 1 / 64,
+    "12" = 1 / 128,
+    "13" = 1 / 256
+  )
   # now the second half timestamp's start from the end of the first half, and now each column is given a team value, depending on the team of the player's coordinates. Also only one event for each time
   dataframe_cleaned <- dataframe %>%
-    mutate(timestamp = if_else(period == 2, as_hms(as.POSIXct(timestamp, format = "%H:%M:%OS")) + difftime(as_hms(as.POSIXct(max_timestamp, format = "%H:%M:%OS")), as_hms(as.POSIXct("00:00:00", format = "%H:%M:%OS"))), as_hms(as.POSIXct(timestamp, format = "%H:%M:%OS")))) %>%
-    mutate(team = ifelse(teammate, team.name, ifelse(team.name == team_names[1], team_names[2], team_names[1]))) %>%
+    mutate(timestamp = if_else(
+      period == 2,
+      as_hms(as.POSIXct(timestamp, format = "%H:%M:%OS")) +
+        difftime(as_hms(
+          as.POSIXct(timestamp_1, format = "%H:%M:%OS")
+        ), as_hms(
+          as.POSIXct("00:00:00", format = "%H:%M:%OS")
+        )),
+      as_hms(as.POSIXct(timestamp, format = "%H:%M:%OS"))
+    ))
+  if (length(unique(dataframe$period)) > 2) {
+    dataframe_cleaned <- dataframe_cleaned %>%
+      mutate(
+        timestamp = if_else(
+          period == 3,
+          as_hms(as.POSIXct(timestamp, format = "%H:%M:%OS")) +
+            difftime(as_hms(
+              timestamp_2
+            ), as_hms(
+              as.POSIXct("00:00:00", format = "%H:%M:%OS")
+            )),
+          as_hms(as.POSIXct(timestamp, format = "%H:%M:%OS"))
+        ),
+        timestamp = if_else(
+          period == 4,
+          as_hms(as.POSIXct(timestamp, format = "%H:%M:%OS")) +
+            difftime(as_hms(
+              timestamp_3
+            ), as_hms(
+              as.POSIXct("00:00:00", format = "%H:%M:%OS")
+            )),
+          as_hms(as.POSIXct(timestamp, format = "%H:%M:%OS"))
+        )
+      )
+  }
+  dataframe_cleaned <- dataframe_cleaned %>%
+    mutate(team = ifelse(
+      teammate,
+      team.name,
+      ifelse(team.name == team_names[1], team_names[2], team_names[1])
+    )) %>%
     group_by(timestamp) %>%
-    filter(id == first(id)) %>%  
-    ungroup() 
+    filter(id == first(id)) %>%
+    ungroup()
   # splits the location into x and y, and makes sure they are between [0, 120] and [0, 80] for x and y respectively. Also turns all of them to be in their defensive area starting at 0
   dataframe_coordinates <- dataframe_cleaned %>%
     mutate(x = map_dbl(location, 1), y = map_dbl(location, 2)) %>%
     mutate(x = pmax(0, pmin(x, 120)), y = pmax(0, pmin(y, 80))) %>%
-    mutate(x = if_else(teammate == FALSE, 120 - x, x), y = if_else(teammate == FALSE, 80 - y, y))
+    mutate(x = if_else(teammate == FALSE, 120 - x, x),
+           y = if_else(teammate == FALSE, 80 - y, y))
   dataframe_zones <- dataframe_coordinates %>%
     rowwise() %>%
-    mutate(zone = case_when(
-      x >= zones$x_min[1] & x < zones$x_max[1] & y >= zones$y_min[1] & y <= zones$y_max[1] ~ 1,
-      x >= zones$x_min[2] & x < zones$x_max[2] & y >= zones$y_min[2] & y <= zones$y_max[2] ~ 2,
-      x >= zones$x_min[3] & x < zones$x_max[3] & y >= zones$y_min[3] & y <= zones$y_max[3] ~ 3,
-      x >= zones$x_min[4] & x < zones$x_max[4] & y >= zones$y_min[4] & y <= zones$y_max[4] ~ 4,
-      x >= zones$x_min[5] & x < zones$x_max[5] & y >= zones$y_min[5] & y <= zones$y_max[5] ~ 5,
-      x >= zones$x_min[6] & x < zones$x_max[6] & y >= zones$y_min[6] & y <= zones$y_max[6] ~ 6,
-      x >= zones$x_min[7] & x < zones$x_max[7] & y >= zones$y_min[7] & y <= zones$y_max[7] ~ 7,
-      x >= zones$x_min[8] & x < zones$x_max[8] & y >= zones$y_min[8] & y <= zones$y_max[8] ~ 8,
-      x >= zones$x_min[9] & x < zones$x_max[9] & y >= zones$y_min[9] & y <= zones$y_max[9] ~ 9,
-      x >= zones$x_min[10] & x < zones$x_max[10] & y >= zones$y_min[10] & y <= zones$y_max[10] ~ 10,
-      x >= zones$x_min[11] & x < zones$x_max[11] & y >= zones$y_min[11] & y <= zones$y_max[11] ~ 11,
-      x >= zones$x_min[12] & x < zones$x_max[12] & y >= zones$y_min[12] & y <= zones$y_max[12] ~ 12,
-      x >= zones$x_min[13] & x <= zones$x_max[13] & y >= zones$y_min[13] & y <= zones$y_max[13] ~ 13)) %>%
+    mutate(
+      zone = case_when(
+        x >= zones$x_min[1] &
+          x < zones$x_max[1] &
+          y >= zones$y_min[1] & y <= zones$y_max[1] ~ 1,
+        x >= zones$x_min[2] &
+          x < zones$x_max[2] &
+          y >= zones$y_min[2] & y <= zones$y_max[2] ~ 2,
+        x >= zones$x_min[3] &
+          x < zones$x_max[3] &
+          y >= zones$y_min[3] & y <= zones$y_max[3] ~ 3,
+        x >= zones$x_min[4] &
+          x < zones$x_max[4] &
+          y >= zones$y_min[4] & y <= zones$y_max[4] ~ 4,
+        x >= zones$x_min[5] &
+          x < zones$x_max[5] &
+          y >= zones$y_min[5] & y <= zones$y_max[5] ~ 5,
+        x >= zones$x_min[6] &
+          x < zones$x_max[6] &
+          y >= zones$y_min[6] & y <= zones$y_max[6] ~ 6,
+        x >= zones$x_min[7] &
+          x < zones$x_max[7] &
+          y >= zones$y_min[7] & y <= zones$y_max[7] ~ 7,
+        x >= zones$x_min[8] &
+          x < zones$x_max[8] &
+          y >= zones$y_min[8] & y <= zones$y_max[8] ~ 8,
+        x >= zones$x_min[9] &
+          x < zones$x_max[9] &
+          y >= zones$y_min[9] & y <= zones$y_max[9] ~ 9,
+        x >= zones$x_min[10] &
+          x < zones$x_max[10] &
+          y >= zones$y_min[10] & y <= zones$y_max[10] ~ 10,
+        x >= zones$x_min[11] &
+          x < zones$x_max[11] &
+          y >= zones$y_min[11] & y <= zones$y_max[11] ~ 11,
+        x >= zones$x_min[12] &
+          x < zones$x_max[12] &
+          y >= zones$y_min[12] & y <= zones$y_max[12] ~ 12,
+        x >= zones$x_min[13] &
+          x <= zones$x_max[13] &
+          y >= zones$y_min[13] & y <= zones$y_max[13] ~ 13
+      )
+    ) %>%
     ungroup()
   dataframe_zones_counts <- dataframe_zones %>%
-    group_by(timestamp, team, zone) %>%
+    group_by(timestamp, team, possession_team.name, zone) %>%
     summarise(count = n(), .groups = 'drop') %>%
-    pivot_wider(names_from = zone, values_from = count, values_fill = 0)
+    pivot_wider(
+      names_from = zone,
+      values_from = count,
+      values_fill = 0
+    )
   dataframe_summarised_zones_counts <- dataframe_zones_counts %>%
     mutate(across(names(scaling_factors), ~ . * scaling_factors[cur_column()])) %>%
     rowwise() %>%
@@ -99,18 +196,44 @@ measure_of_defensive_area_occupied <- function(dataframe) {
     select(-c("1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13"))
 }
 
+list_of_match_ids <- unique(merged_dataframe$match_id)
+
+for (match_id in list_of_match_ids) {
+  dataframe_name <- paste0(match_id, "_mdao_dataframe")
+  if (!exists(dataframe_name)) {
+    filtered_df <- merged_dataframe[merged_dataframe$match_id == match_id, ]
+    mdao_df <- measure_of_defensive_area_occupied(filtered_df)
+    assign(dataframe_name, mdao_df)
+  } 
+}
+
+for (match_id in list_of_match_ids) {
+  dataframe_name <- paste0(match_id, "_mdao_dataframe")
+  average_dataframe_name <- paste0(match_id, "_average_mdao_dataframe")
+  if (exists(dataframe_name) && !exists(average_dataframe_name)) {
+    mdao_df <- get(dataframe_name) 
+    average_mdao_df <- mdao_df %>%
+      group_by(team) %>%
+      summarize(average_row_sum = mean(row_sum, na.rm = TRUE)) %>%
+      mutate(match = match_id)
+    assign(average_dataframe_name, average_mdao_df)
+  }
+}
+
+
+
 # test3 <- measure_of_defensive_area_occupied(test)
-# 
-# ggplot(filter(zone_counts, period == 1), aes(x = timestamp, y = row_sum, colour = team)) + 
+#
+# ggplot(filter(zone_counts, period == 1), aes(x = timestamp, y = row_sum, colour = team)) +
 #   geom_smooth(method = "loess", span = 0.1) +  # Smooth line for running average
 #   theme_minimal() +
 #   labs(title = "Running Average of Row Sum", x = "Timestamp", y = "Row Sum")
-# 
-# ggplot(filter(zone_counts, period == 2), aes(x = timestamp, y = row_sum, colour = team)) + 
+#
+# ggplot(filter(zone_counts, period == 2), aes(x = timestamp, y = row_sum, colour = team)) +
 #   geom_smooth(method = "loess", span = 0.1) +  # Smooth line for running average
 #   theme_minimal() +
 #   labs(title = "Running Average of Row Sum", x = "Timestamp", y = "Row Sum")
-# 
+#
 # team_averages <- zone_counts %>%
 #   group_by(team) %>%
 #   summarize(average_row_sum = mean(row_sum, na.rm = TRUE))
