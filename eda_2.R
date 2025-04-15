077e5bb37bda08d2c89f9d57292e0718e73dfc0e
ElliotJoyce09 <elliot.hill000@gmail.com>
2025-04-15 19:36:55 +0100

    added last change to eda_2.R

--- eda_2.R
+++ eda_2.R
@@ -34,28 +34,40 @@ progressive_action_binned <- binary_modelling_dataframe %>%
 
 binary_modelling_dataframe_summary <- binary_modelling_dataframe %>%
   group_by(shots_occur) %>%
-  summarise(across(c("average_MDAO",
-                     "opposition_average_MDAO",
-                     "number_of_progressive_actions",
-                     "number_of_defender_removing_actions",
-                     "number_of_defenders_removed_from_actions",
-                     "number_of_high_defender_removing_actions",
-                     "number_of_opposition_turnovers",
-                     "number_of_opposition_turnovers_in_their_own_half",
-                     "number_of_successful_pressures",
-                     "number_of_successful_pressures_in_opposition_half"), 
-                   \(x) mean(x, na.rm = TRUE)))
+  summarise(across(
+    c(
+      "average_MDAO",
+      "opposition_average_MDAO",
+      "number_of_progressive_actions",
+      "number_of_defender_removing_actions",
+      "number_of_defenders_removed_from_actions",
+      "number_of_high_defender_removing_actions",
+      "number_of_opposition_turnovers",
+      "number_of_opposition_turnovers_in_their_own_half",
+      "number_of_successful_pressures",
+      "number_of_successful_pressures_in_opposition_half"
+    ),
+    \(x) mean(x, na.rm = TRUE)
+  ))
 
 successful_pressure_binary_dataframe <- binary_modelling_dataframe %>%
-  filter(successful_pressure_closest_to_opposition_goal != max(successful_pressure_closest_to_opposition_goal))
+  filter(
+    successful_pressure_closest_to_opposition_goal != max(successful_pressure_closest_to_opposition_goal)
+  )
 
 binary_modelling_dataframe_successful_pressure_summary <- successful_pressure_binary_dataframe %>%
   group_by(shots_occur) %>%
-  summarise(successful_pressure_closest_to_opposition_goal = mean(successful_pressure_closest_to_opposition_goal, na.rm = TRUE))
+  summarise(
+    successful_pressure_closest_to_opposition_goal = mean(successful_pressure_closest_to_opposition_goal, na.rm = TRUE)
+  )
 
 opposition_turnover_binary_dataframe <- binary_modelling_dataframe %>%
-  filter(opposition_turnover_closest_to_their_own_goal != max(opposition_turnover_closest_to_their_own_goal))
+  filter(
+    opposition_turnover_closest_to_their_own_goal != max(opposition_turnover_closest_to_their_own_goal)
+  )
 
 binary_modelling_dataframe_opposition_turnover_summary <- opposition_turnover_binary_dataframe %>%
   group_by(shots_occur) %>%
-  summarise(opposition_turnover_closest_to_their_own_goal = mean(opposition_turnover_closest_to_their_own_goal, na.rm = TRUE))
+  summarise(
+    opposition_turnover_closest_to_their_own_goal = mean(opposition_turnover_closest_to_their_own_goal, na.rm = TRUE)
+  )
